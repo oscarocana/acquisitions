788d5f05575a7e2f31dd07d2977d4ee005e43ae6
Oscar Ocana <email@oscarocana.com>
2026-04-02 13:35:28 +0100

    added morgan

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import logger from '#config/logger.js';
 import helmet from 'helmet';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   logger.info('Received a request to the root endpoint');

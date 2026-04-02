36372050ae6bb0afd1c5b744cd8a2ad8a63fed56
Oscar Ocana <email@oscarocana.com>
2026-04-02 14:35:21 +0100

    added auth validation

---
+++ src/validations/auth.validaiton.js
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+export const aignUpSchema = z.object({
+  name: z.string().min(1, 'Name is required').max(255, 'Name must be at most 255 characters').trim(),
+  email: z.email('Invalid email format').min(1, 'Email is required').max(255, 'Email must be at most 255 characters').trim(), 
+  password: z.string().min(6, 'Password must be at least 6 characters').max(128, 'Password must be at most 128 characters'),
+  role: z.enum(['user', 'admin']).default('user')
+});
+
+export const signInSchema = z.object({
+  email: z.email().min(2,'Please enter your Password').toLowerCase().trim(),
+  password: z.string().min(1, 'Please enter your Password')
+});

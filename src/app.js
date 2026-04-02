ec177ef40e132a455ac336b8d8d75d229c0f814f
Oscar Ocana <email@oscarocana.com>
2026-04-02 13:32:39 +0100

    added helmet and winston

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import express from 'express';
+import logger from '#config/logger.js';
+import helmet from 'helmet';
 
 const app = express();
 
+app.use(helmet());
+
 app.get('/', (req, res) => {
+  logger.info('Received a request to the root endpoint');
   res.status(200).send('Hello From Acquisitions API!');
 });
 
